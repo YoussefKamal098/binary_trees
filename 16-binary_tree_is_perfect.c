139be50ea7ab539765b538283bce540894efd08e
YoussefKamal098 <kmalywsf098@gmail.com>
2024-04-10 22:04:05 +0200

    is a perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,6 +18,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
 	left = binary_tree_is_perfect(tree->left);
 	right = binary_tree_is_perfect(tree->right);
 
