f98354bb4ff9fb96c7919b741e7264a465296dcf
YoussefKamal098 <kmalywsf098@gmail.com>
2024-04-10 21:27:52 +0200

    balance of the tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,8 +16,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left_height = tree->left ? binary_tree_height(tree->left) : -1;
-	right_height = tree->right ? binary_tree_height(tree->right) : -1;
+	left_height = tree->left ? (int)binary_tree_height(tree->left) : -1;
+	right_height = tree->right ? (int)binary_tree_height(tree->right) : -1;
 
 	return (left_height - right_height);
 }
