cc5d988a4e5baf13d30976d9868388cbc925d978
YoussefKamal098 <kmalywsf098@gmail.com>
2024-04-13 23:55:58 +0200

    update heap_to_sorted_array

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-size_t binary_tree_size(const binary_tree_t *tree);
-
 /**
  * heap_to_sorted_array - Converts a max heap binary tree into a sorted array
  * @heap: Pointer to the root node of the max heap binary tree
@@ -36,24 +34,4 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 	}
 
 	return (array);
-}
-
-/**
- * binary_tree_size - Calculates the size of a binary tree
- * @tree: Pointer to the root node of the binary tree
- *
- * Return: Size of the binary tree, or 0 if @tree is NULL
- *
- * This function calculates the size of a binary tree, which is defined as the
- * total number of nodes in the tree. If the tree is empty or
- * the root node is NULL, the size is considered to be 0. Otherwise,
- * the size is calculated recursively by counting the nodes in the left
- * and right subtrees and adding 1 for the current node.
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
